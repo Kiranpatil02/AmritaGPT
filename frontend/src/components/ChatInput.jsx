16f4550db1596f62a37e52e7637308527e6cdf96
Vijay SB <vijaysb2006@gmail.com>
2024-12-22 18:17:09 +0530

    Responsiveness added

--- frontend/src/components/ChatInput.jsx
+++ frontend/src/components/ChatInput.jsx
@@ -8,7 +8,7 @@ const ChatInput = () => {
       <div className="bg-[#A4123F] p-4 rounded-full">
         <FaMicrophone size={25} color="white"/>
       </div>
-      <input type="text" placeholder="Ask me anything about Amrita Vishwa Vidyapeetham!" className="w-full mx-6 py-4 px-6 rounded-lg outline-none text-lg"/>
+      <input type="text" placeholder="Ask me anything about Amrita Vishwa Vidyapeetham!" className="w-full mx-6 py-4 px-6 rounded-lg outline-none text-sm md:text-lg"/>
       <div className="bg-white rounded-xl p-4">
         <IoSend size={25}/>
       </div>
--- frontend/src/components/UserChat.jsx
+++ frontend/src/components/UserChat.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const UserChat = ({message}) => {
   return (
-    <div className='bg-white py-3 px-6 rounded-2xl max-w-[50%] w-fit ml-auto my-2 mx-6'>
+    <div className='bg-white py-3 px-6 rounded-2xl max-w-[65%] w-fit ml-auto my-2 text-sm '>
       {message}
     </div>
   )
