baaad01709dbf176f7bda5a01b059a3271046fe0
Kiran <Kiranpatil73827@gmail.com>
2024-12-25 00:20:30 +0530

    Updated

--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -5,9 +5,9 @@ import GPTChat from "./GPTChat";
 const MessageContainer = () => {
   return (
     <div className="px-6 mb-4">
-      <GPTChat message={"Hey im amrita gpt"} />
-      <UserChat message={"Tell me about the clubs in Amrita!"} />
-      <GPTChat message={"Based on the provided context, here are all the clubs in Amrita that I can find information about:  IETE Club at Amrita, Google Developer Student Clubs Amrita, Srishti - The literary club, Natyasudha (Dance Club), Ragasudha (Music Club)"} />
+      <GPTChat message={"Hey I am Amrita-gpt, Ask any queries related to Amrita Univeristy"} />
+      {/* <UserChat message={"Tell me about the clubs in Amrita!"} /> */}
+      {/* <GPTChat message={"Based on the provided context, here are all the clubs in Amrita that I can find information about:  IETE Club at Amrita, Google Developer Student Clubs Amrita, Srishti - The literary club, Natyasudha (Dance Club), Ragasudha (Music Club)"} /> */}
     </div>
   );
 };
