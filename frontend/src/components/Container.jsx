5b062de8c49166da3f7225fcf1878ccf20dc0714
Kiran <Kiranpatil73827@gmail.com>
2024-12-25 14:54:13 +0530

    Text overflow fix

--- frontend/src/components/Container.jsx
+++ frontend/src/components/Container.jsx
@@ -5,7 +5,7 @@ import MessageContainer from "./MessageContainer";
 
 const Container = () => {
   return (
-    <div className="bg-[#DAD9D9] md:w-[70%] sm:w-[90%] mx-auto h-[80vh] rounded-2xl p-10 mt-2 relative">
+    <div className="bg-[#DAD9D9] md:w-[70%] sm:w-[90%] mx-auto h-[80vh] rounded-2xl p-10 mt-2 relative overflow-y-hidden">
       <div className="w-full absolute bottom-6 left-0 right-0 mx-auto px-6">
         <MessageContainer/>
         <ChatInput />
