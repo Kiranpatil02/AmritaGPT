a9a3c877168d190146685a5d720cc3f25633fc3f
Kiran <Kiranpatil73827@gmail.com>
2024-12-25 11:36:13 +0530

    handled submission

--- frontend/src/components/ChatInput.jsx
+++ frontend/src/components/ChatInput.jsx
@@ -12,7 +12,8 @@ const ChatInput = () => {
   function Inputchange(e){
     setquery(e.target.value);
   }
-  function handlesubmit(){
+  function handlesubmit(e){
+    e.preventDefault()
     if(query.trim()!=""){
 
       setquestion((prev)=>[...prev,query]);
@@ -21,6 +22,8 @@ const ChatInput = () => {
   }
   return (
     <>
+    <form onSubmit={handlesubmit}>
+
     <div>
       {question.map((q,index)=>(
         <UserChat key={index} message={q} duration={setdelay} />
@@ -28,16 +31,18 @@ const ChatInput = () => {
     </div>
 
     <div className="flex p-3 items-center justify-between">
-      <div className="bg-[#A4123F] p-4 rounded-full">
+      <div className="bg-[#A4123F] p-4 rounded-full hover:cursor-pointer">
         <FaMicrophone size={25} color="white"/>
       </div>
       <input type="text" placeholder="Ask me anything about Amrita Vishwa Vidyapeetham!" value={query} className="w-full mx-6 py-4 px-6 rounded-lg outline-none text-lg" onChange={Inputchange} disabled={delay} />
       <div className="bg-white rounded-xl p-4">
-        <button onClick={handlesubmit} disabled={delay}>
+        <button type="submit"  disabled={delay}>
         <IoSend className="hover:cursor-pointer" size={25}/>
         </button>
       </div>
     </div>
+    </form>
+
     </>
   );
 };
